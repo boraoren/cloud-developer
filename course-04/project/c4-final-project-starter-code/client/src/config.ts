42b956452ec627c39ae1423897b608155537f990
bora <bora@boras-MacBook-Pro.local>
2020-02-25 15:19:10 +1100

    configure package.json

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'boren.au.auth0.com',
+  clientId: 'RqryuU1tEMjDX1FUdQ3hqnpnzYrn5fVB',
   callbackUrl: 'http://localhost:3000/callback'
 }
